b062d55f35982ee31dfb45ba789768e6eebbe16e
palomaalmeida09 <paloma.monteiro@escolar.ifrn.edu.br>
2023-12-27 11:08:38 -0300

    alterações

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets 
+from .models import Artista, Album, Musica
+from .serializers import ArtistaSerializer, AlbumSerializer, MusicaSerializer
 
-# Create your views here.
+class ArtistaViewSet(viewsets.ModelViewSet):
+    """
+    Permite que o usuário manipule os dados Artista
+    """
+    queryset = Artista.objects.all()
+    serializer_class = ArtistaSerializer
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    """
+    Permite que o usuário manipule os dados de Albuns
+    """
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class MusicaViewSet(viewsets.ModelViewSet):
+    """
+    Permite que o usuário manipule os dados Artista
+    """
+    queryset = Musica.objects.all()
+    serializer_class = MusicaSerializer
